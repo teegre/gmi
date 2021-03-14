a1e34ffb27dd44ae3df3d7d86e244e3a5ef84b16
Stéphane MEYER <tigerlost@me.com>
2021-03-14 18:14:27 +0100

    Added "articles" as a distinct command.

--- src/list.sh
+++ src/list.sh
@@ -10,10 +10,29 @@
 #
 # LIST
 # C: 2021/03/13
-# M: 2021/03/13
+# M: 2021/03/14
 # D: Display a list menu.
 
 source /usr/lib/gmi/core.sh
+
+
+article_list() {
+  local entry title article
+  declare -A alist
+  while read -r entry; do
+    title="$(get_title "$entry")"
+    alist["$title"]="$entry"
+  done < <(find "$src" -not \( -path "${src}"archive -prune \) -regex '^.+/[0-9]+.+/index\.gmi$' | sort)
+  article="$(
+  (
+    for entry in "${!alist[@]}"; do
+      echo "$entry"
+    done
+    echo quit
+  ) | fzf +s --tac --header "gmi version $__version")"
+  [[ $article == "quit" ]] && { echo "quit"; return 0; }
+  echo "${alist["$article"]}"
+}
 
 list() {
   # list options and articles.
@@ -27,7 +46,7 @@ list() {
       echo "new"
       echo "post"
       echo "deploy"
-      find "$src" -not \( -path "${src}"archive -prune \) -regex '^.+/[0-9]+.+/index\.gmi$' | sort
+      echo "articles"
       echo "index"
       echo "micro"
       echo "archive-index"
@@ -35,6 +54,7 @@ list() {
       echo "auto-archive"
       echo "quit"
   ) | fzf +s --tac --header "gmi version $__version")"
+  
   echo "$entry"
 }
 
